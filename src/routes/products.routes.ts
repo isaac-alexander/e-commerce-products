5eb84826ccd7188436007e8340a7df91e21a15de
Isaac Alexander <Isaacalexander81@gmail.com>
2025-09-09 01:59:16 +0100

    src/routes/products.routes.ts

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,19 +1,12 @@
-import { Router } from 'express';
-import {
-    createProduct,
-    getAllProducts,
-    getProductById,
-    updateProduct,
-    deleteProduct,
-} from '../controllers/product.controller';
-
+import { Router } from "express";
+import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from "../controllers/product.controller";
 
 const router = Router();
-router.post('/', createProduct);
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
 
+router.post("/products", createProduct);
+router.get("/products", getAllProducts);
+router.get("/products/:id", getProductById);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
