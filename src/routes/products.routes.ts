99798d538854f1efa5ba2986374bf98c89af30d7
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-03 02:03:41 +0100

    src/routes/products.routes.ts

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from "../controllers/product.controller";
+import { authenticate, authorizeAdmin } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.post("/", createProduct);
-router.get("/", getAllProducts);
-router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+// Customers and Admin
+router.get("/products", authenticate, getAllProducts);
+router.get("/products/:id", authenticate, getProductById);
+
+// Admins
+router.post("/products", authenticate, authorizeAdmin, createProduct);
+router.put("/products/:id", authenticate, authorizeAdmin, updateProduct);
+router.delete("/products/:id", authenticate, authorizeAdmin, deleteProduct);
 
 export default router;
